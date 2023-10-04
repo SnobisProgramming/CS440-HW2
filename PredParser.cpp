aa9d3428bd561d5017a7e6f93518ff3c0446cb54
snobis <lbrewer@iwu.edu>
2023-10-04 15:56:03 -0500

    Implemented a .cpp file for PredParser. Remove Node and Token .cpp files - I wanted to do those too, but need to focus on the assigntment itself first. If I have time after completing HW specs, I may go back to it.

---
+++ PredParser.cpp
@@ -0,0 +1,145 @@
+/*
+PredParser.cpp
+*/
+
+
+#include "PredParser.h"
+#include "Node.h"
+#include "Token.h"
+#include <string>
+
+// Constructor
+PredParser::PredParser() {
+    // Lookahead starts at beginning of token vector
+    lookahead = toks.begin();
+}
+
+PredParser::PredParser(std::vector<Token*> &input_toks) {
+    toks = input_toks;
+    // Lookahead starts at beginning of token vector
+    lookahead = toks.begin();
+}
+
+// Prints ParseTree
+void PredParser::print() {
+
+}
+
+// Does a top down parse
+// Call Stmt()?
+void PredParser::top_down_parse() {
+
+}
+
+// match is for leaf tokens
+// check if lookahead not at end, check if lookahead is correct token, if so create leaf and push ahead
+// never mind, not implemented.  I would need a million cases to cast appropriate token depending on tag
+// Is there a way to detect the type of an object passed?
+//Node* match(int t, string s);
+
+// if there is a way (polymorphic equal?), we could instead do:
+//Node* match()
+
+// if not, then function probably should be
+//Node* match(int t, string s, string type)?
+
+// Stmt checks for if.  If not found, go with Rel production instead
+Node* PredParser::Stmt() {
+    // creation of the parent node for the statement
+    Node* output_tree = new Node("S");
+          
+        // Check if lookahead token is "if"
+        if ((*lookahead)->tag() == IF) {
+            // Create new node for the if statement
+            Node* ifStmt = new Node("if");
+            // Lookahead
+            lookahead++;
+
+            // According to our grammar, (Rel) is next
+            // Thus, check for (
+            if ((*lookahead)->lexeme == "(") {
+                // Lookahead upon confirming (
+                lookahead++;
+
+                // We should have Rel; parse Rel
+                Node* relNode = Relop();
+
+                // Rel is a child of an if statement
+                ifStmt->children.push_back(relNode);
+
+                // According to our grammar, (Rel) ends in a )
+                // Thus, check for )
+                if ((*lookahead)->lexeme == ")") {
+                // Lookahead upon confirming )
+                lookahead++;
+
+                // We should have (then) Stmt; parse Stmt
+                Node* thenStmt = Stmt();
+
+                // (then) Stmt is a child of an if statement
+                ifStmt->children.push_back(thenStmt);
+
+                // Check for else
+                // question - is else required in our grammar?
+                    if ((*lookahead)->tag == ELSE) {
+                        // Look ahead
+                        lookahead++;
+
+                        // We should have Stmt; parse Stmt
+                        Node* elseStmt = Stmt();
+
+                        // (else) Stmt is a child of an if statement
+                        ifStmt->children.push_back(elseStmt)
+                }
+
+                    // Finished parsing ifStmt
+                    return ifStmt;
+            
+                }
+                // case: ) missing
+                else {
+                    // error
+                }
+            }
+                // case: ( missing
+                else {
+                    // error
+            }
+        }
+    // other cases, such as Rel
+
+    // Return parent node for Stmt
+    return output_tree;
+}
+
+// Rel only has one choice, to go to Exp Relop
+Node* PredParser::Rel();
+
+// Boolop checks for < or >, if neither found go with epsilon
+Node* PredParser::Boolop();
+
+// Start with Exp, only one production, namely Term Add
+Node* PredParser::Exp();
+
+// Three choices for Add, which depends on the lookahead token
+Node* PredParser::Add();
+
+// If Term is current nonterminal, only one production to choose, namely Factor Mul
+Node* PredParser::Term();
+
+// For Mul, apply * Factor Mul or / Factor Mul depending on lookahead, o/w do epsilon
+Node* PredParser::Mul();
+
+//If lookahead is num or var, return leaf node, otherwise return tree corresponding to (Exp)
+Node* PredParser::Factor();
+
+/* Here is the grammar that this parser will parse:
+Stmt -> Rel | if ( Rel ) Stmt else Stmt 
+Rel -> Exp Boolop
+Boolop -> < Exp | > Exp | epsilon
+Exp -> Term Add
+Add -> + Term Add | - Term Add | epsilon
+Term -> Factor Mul
+Mul -> * Factor Mul | / Factor Mul | epsilon
+Factor -> num | var | true | false | ( Exp )
+*/
--- PredParser.h
+++ PredParser.h
@@ -8,130 +8,27 @@ Designing a simple predictive parser for arithmetic, following Chap 2 of the Dra
 
 #include "Node.h"
 #include "Token.h"
-#include <string>
+#include <vector>
 
-class PredParser{
-    public:
-        ParseTree pt;
-        vector<Token*> toks;
-
-        // lookahead is an iterator that points at the next token in toks
-        vector<Token*>::iterator lookahead;
-
+class PredParser {
     public:
         PredParser();
-        PredParser(vector<Token*> &input_toks);
-
-        // print calls print function on the ParseTree
+        PredParser(std::vector<Token*> &input_toks);
         void print();
-
-        // parse function fills in pt based on tokens in toks
         void top_down_parse();
-
-        // match is for leaf tokens
-        // check if lookahead not at end, check if lookahead is correct token, if so create leaf and push ahead
-        // never mind, not implemented.  I would need a million cases to cast appropriate token depending on tag
-        // Is there a way to detect the type of an object passed?
-        //Node* match(int t, string s);
-
-        // if there is a way (polymorphic equal?), we could instead do:
-        //Node* match()
-
-        // if not, then function probably should be
-        //Node* match(int t, string s, string type)?
-
-        // Stmt checks for if.  If not found, go with Rel production instead
-        Node* Stmt() {
-          // creation of the parent node for the statement
-          Node* output_tree = new Node("S");
-          
-          // Check if lookahead token is "if"
-          if ((*lookahead)->tag() == IF) {
-            // Create new node for the if statement
-            Node* ifStmt = new Node("if");
-            // Lookahead
-            lookahead++;
-          }
-
-          // According to our grammar, (Rel) is next
-          // Thus, check for (
-          if ((*lookahead)->lexeme == "(") {
-            // Lookahead upon confirming (
-            lookahead++;
-
-            // We should have Rel; parse Rel
-            Node* relNode = Relop();
-
-            // Rel is a child of an if statement
-            ifStmt->children.push_back(relNode);
-
-            // According to our grammar, (Rel) ends in a )
-            // Thus, check for )
-            if ((*lookahead)->lexeme == ")") {
-              // Lookahead upon confirming )
-              lookahead++;
-
-              // We should have (then) Stmt; parse Stmt
-              Node* thenStmt = Stmt();
-
-              // (then) Stmt is a child of an if statement
-              ifStmt->children.push_back(thenStmt);
-
-              // Check for else
-              // question - is else required in our grammar?
-              if ((*lookahead)->tag == ELSE) {
-                // Look ahead
-                lookahead++;
-
-                // We should have Stmt; parse Stmt
-                Node* elseStmt = Stmt();
-
-                // (else) Stmt is a child of an if statement
-                ifStmt->children.push_back(elseStmt)
-              }
-
-              // Finished parsing ifStmt
-              return ifStmt;
-          
-            }
-            // if needed
-            else {
-              // case: ) missing
-            }
-          else {
-            // case: ( missing
-          }
-        else {
-          // return relop?
-          
-        }
-        }
-        }
-        // Rel only has one choice, to go to Exp Relop
+        Node* Stmt();
         Node* Rel();
-        // Boolop checks for < or >, if neither found go with epsilon
         Node* Boolop();
-        // Start with Exp, only one production, namely Term Add
         Node* Exp();
-        // Three choices for Add, which depends on the lookahead token
         Node* Add();
-        // If Term is current nonterminal, only one production to choose, namely Factor Mul
         Node* Term();
-        // For Mul, apply * Factor Mul or / Factor Mul depending on lookahead, o/w do epsilon
         Node* Mul();
-        //If lookahead is num or var, return leaf node, otherwise return tree corresponding to (Exp)
         Node* Factor();
+    
+    private:
+        ParseTree pt;
+        std::vector<Token*> toks;
+        std::vector<Token*>:iterator lookahead;
 };
-
-/* Here is the grammar that this parser will parse:
-Stmt -> Rel | if ( Rel ) Stmt else Stmt 
-Rel -> Exp Boolop
-Boolop -> < Exp | > Exp | epsilon
-Exp -> Term Add
-Add -> + Term Add | - Term Add | epsilon
-Term -> Factor Mul
-Mul -> * Factor Mul | / Factor Mul | epsilon
-Factor -> num | var | true | false | ( Exp )
-*/
 
 #endif
